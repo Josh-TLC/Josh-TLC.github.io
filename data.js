54bd75ced3257599e98747d6728310973c472064
Joshua Wharton <josh@thelinkingcompany.com>
2019-10-25 15:31:14 +0100

    fixed commas

--- data.js
+++ data.js
@@ -1,67 +1,64 @@
 // Questions
 var q = [
-'What are you making?'
-'Won’t this just become a dating app?'
-'How are you going to market it?'
-'Ok so what does that mean?'
-'Like what type of marketing?'
-'Surely university is the place for meeting new people / no routine?'
-'If they’re too disinterested in joining societies, then why would they commit to the app?'
-'Are you open to dating?'
-'Are you open to group meetups?'
-'Why instantaneous?'
-'What’s new about what you’re making??'
-'Who needs what you’re making?'
-'How do you know they need it?'
-'What are they doing now?'
-'What, exactly, makes you different from existing options?'
-'How does your product work in more detail?'
-'How many people are in your target market?'
-'Who are your competitors? Who might become competitors?'
-'Which competition do you fear most?'
-'What are some trends in your target market, and related markets?'
-'Why will you succeed over others? What do you understand that others don’t?'
-'What are current alternatives?'
-'Why is nobody else doing this?'
-'If your startup succeeds, what additional areas might you be able to expand into?'
-'How do you see your product evolving as you gain users?'
-'How long can you go before funding?'
-'What will you do if we don’t fund you?'
-'Why did your team get together?'
-'How did your team get together?'
-'Who is “the boss”?'
-'How do we know your team will stick together?'
-'Who in your team does what?'
-'Who will be your next hire?'
-'Biggest argument between co founders.'
-'Why did you choose this idea?'
-'Why are you uniquely qualified to work on this?'
-'Why do you want to dedicate your life to working on this?'
-'Tell us something surprising that has happened.'
-'What’s the biggest mistake you have made?'
-'Are you open to changing your idea?'
-'Isn’t this a bit creepy?'
-'How far along are you?'
-'Who is going to be your first customer?'
-'How much money do you need to do what you want to do?'
-'How will you make money?'
-'What makes new users try you?'
-'How do you get users?'
-'What resistance will users have to trying it out and how will you overcome it?'
-'Six months from now, what’s going to be your biggest problem?'
-'Why university students first?'
-'How are you going to deal with people just instantly not getting along?'
-'What’s the business model?'
-'How will you retain users: once someone has made a few friends they will no longer need to use the app?'
-'What metric will you measuee to track your growth?'
-'What will you do in 6 months time if no one is using the app?'
-'Is this just for lonely people?'
-'What if people press linkup and then never go to it?'
-'What if people keep not going to linkups?'
+'What are you making?',
+'Won’t this just become a dating app?',
+'How are you going to market it?',
+'Ok so what does that mean?',
+'Like what type of marketing?',
+'Surely university is the place for meeting new people / no routine?',
+'If they’re too disinterested in joining societies, then why would they commit to the app?',
+'Are you open to dating?',
+'Are you open to group meetups?',
+'Why instantaneous?',
+'What’s new about what you’re making??',
+'Who needs what you’re making?',
+'How do you know they need it?',
+'What are they doing now?',
+'What, exactly, makes you different from existing options?',
+'How does your product work in more detail?',
+'How many people are in your target market?',
+'Who are your competitors? Who might become competitors?',
+'Which competition do you fear most?',
+'What are some trends in your target market, and related markets?',
+'Why will you succeed over others? What do you understand that others don’t?',
+'What are current alternatives?',
+'Why is nobody else doing this?',
+'If your startup succeeds, what additional areas might you be able to expand into?',
+'How do you see your product evolving as you gain users?',
+'How long can you go before funding?',
+'What will you do if we don’t fund you?',
+'Why did your team get together?',
+'How did your team get together?',
+'Who is “the boss”?',
+'How do we know your team will stick together?',
+'Who in your team does what?',
+'Who will be your next hire?',
+'Biggest argument between co founders.',
+'Why did you choose this idea?',
+'Why are you uniquely qualified to work on this?',
+'Why do you want to dedicate your life to working on this?',
+'Tell us something surprising that has happened.',
+'What’s the biggest mistake you have made?',
+'Are you open to changing your idea?',
+'Isn’t this a bit creepy?',
+'How far along are you?',
+'Who is going to be your first customer?',
+'How much money do you need to do what you want to do?',
+'How will you make money?',
+'What makes new users try you?',
+'How do you get users?',
+'What resistance will users have to trying it out and how will you overcome it?',
+'Six months from now, what’s going to be your biggest problem?',
+'Why university students first?',
+'How are you going to deal with people just instantly not getting along?',
+'What’s the business model?',
+'How will you retain users: once someone has made a few friends they will no longer need to use the app?',
+'What metric will you measuee to track your growth?',
+'What will you do in 6 months time if no one is using the app?',
+'Is this just for lonely people?',
+'What if people press linkup and then never go to it?',
+'What if people keep not going to linkups?',
 'Why would lonely people be bothered to use this if they aren’t bothered to find friends in other ways?'
-
-
-
 
 
 ]
